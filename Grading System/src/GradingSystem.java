1567f33352f4c3e68cf86b9bf1436bbc2752f033
Patel Dharam Hiteshbhai <147646116+Dharam13@users.noreply.github.com>
2024-08-04 19:31:59 +0530

    GradingSystem.java

--- Grading System/src/GradingSystem.java
+++ Grading System/src/GradingSystem.java
@@ -1,37 +1,69 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class GradingSystem {
-    private List<Student> students;
-    private List<Grade> grades;
-    private Map<Integer, Integer> courseCredits;
+    private Student[] students;
+    private Grade[] grades;
+    private int[] courseIDs;
+    private int[] courseCredits;
+
+    private int studentCount;
+    private int gradeCount;
+    private int courseCount;
+
+    private static final int MAX_STUDENTS = 100;
+    private static final int MAX_GRADES = 500;
+    private static final int MAX_COURSES = 50;
 
     public GradingSystem() {
-        students = new ArrayList<>();
-        grades = new ArrayList<>();
-        courseCredits = new HashMap<>();
+        students = new Student[MAX_STUDENTS];
+        grades = new Grade[MAX_GRADES];
+        courseIDs = new int[MAX_COURSES];
+        courseCredits = new int[MAX_COURSES];
+
+        studentCount = 0;
+        gradeCount = 0;
+        courseCount = 0;
     }
 
     public void addStudent(Student student) {
-        students.add(student);
+        if (studentCount < MAX_STUDENTS) {
+            students[studentCount++] = student;
+        } else {
+            System.out.println("Maximum number of students reached.");
+        }
     }
 
     public void addGrade(Grade grade) {
-        grades.add(grade);
+        if (gradeCount < MAX_GRADES) {
+            grades[gradeCount++] = grade;
+        } else {
+            System.out.println("Maximum number of grades reached.");
+        }
     }
 
     public void addCourseCredits(int courseID, int credits) {
-        courseCredits.put(courseID, credits);
+        if (courseCount < MAX_COURSES) {
+            courseIDs[courseCount] = courseID;
+            courseCredits[courseCount++] = credits;
+        } else {
+            System.out.println("Maximum number of courses reached.");
+        }
+    }
+
+    private int getCourseCredits(int courseID) {
+        for (int i = 0; i < courseCount; i++) {
+            if (courseIDs[i] == courseID) {
+                return courseCredits[i];
+            }
+        }
+        return 0; // default if courseID is not found
     }
 
     public double calculateGPA(int studentID) {
         int totalCredits = 0;
         int totalPoints = 0;
-        for (Grade grade : grades) {
+        for (int i = 0; i < gradeCount; i++) {
+            Grade grade = grades[i];
             if (grade.getStudentID() == studentID) {
-                int credits = courseCredits.get(grade.getCourseID());
+                int credits = getCourseCredits(grade.getCourseID());
                 totalCredits += credits;
                 totalPoints += gradeToPoints(grade.getGrade()) * credits;
             }
@@ -52,7 +84,8 @@ public class GradingSystem {
 
     public void printGradeReport(int studentID) {
         System.out.println("Grade Report for Student ID: " + studentID);
-        for (Grade grade : grades) {
+        for (int i = 0; i < gradeCount; i++) {
+            Grade grade = grades[i];
             if (grade.getStudentID() == studentID) {
                 System.out.println("Course ID: " + grade.getCourseID() + ", Grade: " + grade.getGrade());
             }
